8ed03c66ff9296ac7ae99f173f7207809e4c1918
sydney <sydneyosindi03@gmail.com>
2025-07-31 01:33:18 +0300

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import Header from "./components/Header";
-import About from "./components/About";
-import ArticleList from "./components/ArticleList";
-import { blogData } from "./data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import { blogData } from "../data/blog";
 
 function App() {
   const { name, image, about, posts } = blogData;
